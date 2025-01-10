aaa2e2e24f82a56f1937b181584dbb6402b8587c
JOSE CASTA <enriquecasta117@gmail.com>
2025-01-10 13:34:57 -0600

    CREANDO EL METODO ATACAR

--- POO_2C.py
+++ POO_2C.py
@@ -9,11 +9,11 @@ class personaje:
 #variable del constructor vacio 
 mi_personaje = personaje()
 #modificando valores de los atributos
-mi personaje.nombre = "EstebanDido"
-mi personaje.fuerza = 300
-mi personaje.inteligencia = -2
-mi personaje.defensa = 30
-mi personaje.vida = 2
+mi_personaje.nombre = "EstebanDido"
+mi_personaje.fuerza = 300
+mi_personaje.inteligencia = -2
+mi_personaje.defensa = 30
+mi_personaje.vida = 2
 
 
 
