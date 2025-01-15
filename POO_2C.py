ae70854e375bcca47ec8b9185ed87025a9d2cb42
JOSE CASTA <enriquecasta117@gmail.com>
2025-01-15 12:40:35 -0600

    SIMULANDO ENCAPSULACIÓN

--- POO_2C.py
+++ POO_2C.py
@@ -3,10 +3,10 @@ from kivy.uix.accordion import ObjectProperty
 class personaje:
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
         self.nombre = nombre 
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.defensa = defensa 
-        self.vida = vida
+        self.__fuerza = fuerza
+        self.__inteligencia = inteligencia
+        self.__defensa = defensa 
+        self.__vida = vida
         
 #PREGUNTA DE EXAMEEEEEEEENNNNNN::::::: QUE ES SELF??? SELF ES UNA REFERENCIA AL MISMO OBJETO 
 #### PREGUNTA DE EXAMEN, QUE ES EL MEOTOD INIT ?????? CONTRUCTOR QUE INICIALIZA LOS ATRIBUTOS DE UN ObjeTO
@@ -22,38 +22,58 @@ class personaje:
 #variable del constructor vacio 
     def imprimir_atributos(self):
       print(self.nombre)
-      print("-Fuerza:",self.fuerza)
-      print("-Inteligencia:",self.inteligencia)
-      print("-defensa",self.defensa)
-      print("-vida:",self.vida)
+      print("-Fuerza:",self.__fuerza)
+      print("-Inteligencia:",self.__inteligencia)
+      print("-defensa",self.__defensa)
+      print("-vida:",self.__vida)
       
     def subir_nivel(self, fuerza, inteligencia, defensa):
-        self.fuerza = self.fuerza + fuerza
-        self.inteligencia = self.inteligencia + inteligencia 
-        self.defensa = self.defensa + defensa
+        self.__fuerza = self.__fuerza + fuerza
+        self.__inteligencia = self.__inteligencia + inteligencia 
+        self.__defensa = self.__defensa + defensa
     
     
     def esta_vivo(self):
-        return self.vida > 0
+        return self.__vida > 0
     
     def morir(self):
-        self.vida = 0
+        self.__vida = 0
         print(self.nombre,"ha muerto")
         
     def dañar(self, enemigo): 
-        return self.fuerza - enemigo.defensa 
+        return self.__fuerza - enemigo.__defensa 
     
     def atacar(self,enemigo):
         daño = self.dañar(enemigo)
-        enemigo.vida= enemigo.vida - daño 
+        if daño < 0:
+            daño = 0
+        if enemigo.__vida - daño < 0: 
+            enemigo.__vida = 0
+        else: 
+            enemigo.__vida= enemigo.__vida - daño 
         print (self.nombre,"ha realizado",daño, "puntos de daño a", enemigo.nombre)
-        print("Vida de",enemigo.nombre,"es",enemigo.vida)
+        print("Vida de",enemigo.nombre,"es",enemigo.__vida)
     
-mi_personaje = personaje("EsteBanDiDo", 100, 50, 45, 100)
+    def get_vida(self):
+        return self.__vida 
+    
+    def set_vida(self, vida):
+        self.__vida = vida 
+        if self.__vida <= 0:
+            self.morir()
+        
+        
+        
+mi_personaje = personaje("EsteBanDiDo", 100, 50, 45, 100) 
 #mi_personaje.morir()
 mi_enemigo = personaje("Angel", 70, 100, 40, 100)
-mi_personaje.imprimir_atributos()
-mi_personaje.atacar(mi_enemigo)
+print(mi_personaje.get_vida())
+mi_personaje.set_vida(-5)
+mi_personaje._personaje_vida = -50
+print(mi_personaje.get_vida())
+#mi_personaje.vida = 0
+#mi_personaje.imprimir_atributos()
+#mi_personaje.atacar(mi_enemigo)
 #print(mi_personaje.esta_vivo())
 #mi_personaje.subir_nivel(15,5,10)
 #print ("valores actulizados")
@@ -63,13 +83,13 @@ mi_personaje.atacar(mi_enemigo)
 
 #modificando valores de los atributos
 #mi_personaje.nombre = "EstebanDido"
-#mi_personaje.fuerza = 300
-#mi_personaje.inteligencia = -2
-#mi_personaje.defensa = 30
-#mi_personaje.vida = 2
+#mi_personaje.__fuerza = 300
+#mi_personaje.__inteligencia = -2
+#mi_personaje.__defensa = 30
+#mi_personaje.__vida = 2
 
 #print("El nombre de mi personaje es: ",mi_personaje.nombre)
-#print("El fuerza de mi personaje es: ",mi_personaje.fuerza)
-#print("El inteligencia de mi personaje es: ",mi_personaje.inteligencia)
-#print("El defensa de mi personaje es: ",mi_personaje.defensa)
-#print("El vida de mi personaje es: ",mi_personaje.vida)
+#print("El fuerza de mi personaje es: ",mi_personaje.__fuerza)
+#print("El inteligencia de mi personaje es: ",mi_personaje.__inteligencia)
+#print("El defensa de mi personaje es: ",mi_personaje.__defensa)
+#print("El vida de mi personaje es: ",mi_personaje.__vida)
